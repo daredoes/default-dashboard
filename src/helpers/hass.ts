b57394d85c571efa06b6edbd29579e4eb4a2926b
Daniel Evans <me@daredoes.work>
2022-11-16 23:55:40 +0000

    bump yarn lock

--- src/helpers/hass.ts
+++ src/helpers/hass.ts
@@ -1,7 +1,6 @@
 import { HomeAssistant } from 'custom-card-helpers';
-import { LitElement } from 'lit';
 
 export const getHass = (): HomeAssistant => {
-  const hass = document.getElementsByTagName('home-assistant')[0] as LitElement;
+  const hass = document.getElementsByTagName('home-assistant')[0];
   return (hass as any).hass as HomeAssistant;
 };
