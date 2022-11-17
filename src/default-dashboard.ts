b46b5dca1fdf22a3b22b1bcd23743498e74370fa
Daniel Evans <me@daredoes.work>
2022-11-17 00:02:38 +0000

    Set enabled as default after setting options

--- src/default-dashboard.ts
+++ src/default-dashboard.ts
@@ -33,6 +33,14 @@ let controller: Controller;
         },
         { entity_id: 'input_select.default_dashboard' },
       );
+      hass.callService(
+        'input_select',
+        'select_option',
+        {
+          option: 'enabled',
+        },
+        { entity_id: 'input_select.default_dashboard' },
+      );
       log('Reloading the page for new options');
       location.replace('/');
       return;
