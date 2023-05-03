0d7e48eab8abbf734c81c5e765ce3632400c4497
Patrick Li <patrick@papaq.org>
2023-05-03 19:44:24 +1200

    Remove debug code

--- src/controller.ts
+++ src/controller.ts
@@ -19,7 +19,6 @@ class DefaultDashboardController {
       type: 'input_boolean/create',
       name: 'Default Dashboard',
     });
-    console.log(res);
     return res;
   };
 
@@ -36,7 +35,6 @@ class DefaultDashboardController {
       name: 'Default Dashboard',
       options: ['lovelace', ...dashboards, 'refresh'],
     });
-    console.log(res);
     return res;
   };
 
